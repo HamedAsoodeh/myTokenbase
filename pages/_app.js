4809d9c36313b959f4b54b4c5ce3c843bf76ea9d
Hamed Asoodeh <92183756+HamedAsoodeh@users.noreply.github.com>
2023-10-10 07:14:35 -1200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,5 +3,6 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
+export default MyAppd
+export default dMyApp
 export default MyApp
