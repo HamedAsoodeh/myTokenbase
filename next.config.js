3e0c272661706c1cbac8fd3aa5df97b255cdaacf
Hamed Asoodeh <92183756+HamedAsoodeh@users.noreply.github.com>
2023-10-20 04:30:29 -1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 import nextConfig {
   const nextConfig = false,
     reactStrictMode: false,
+    
+}
+import Jscofig = {
+  const jsconfig = {
+    first js = 55`;
+  }
 }
 const nextConfig = {
  FirstLasttenn <= module.exports = 
